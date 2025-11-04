b8b7206e8ef9abcd8c520ee5b81caeb41d699044
sudomastery <roykoigu@protonmail.com>
2025-11-04 07:10:15 +0300

    Add basic routes for index, parameter printing, counting, and math operations
    - Implemented a default route that returns a welcome message.
    - Added a route to print a passed string parameter.
    - Created a counting route that displays numbers up to a specified integer.
    - Introduced a math route to perform basic arithmetic operations based on the provided operation type.

--- server/app.py
+++ server/app.py
@@ -4,5 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#setup the basic routes
+@app.route('/')
+def index():
+    return '<h1>This is the default route</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_parameter(parameter):
+    return f'<h1>The paramter is: {parameter}</h1>'
+
+@app.route('/count/<int:number>')
+def count_number(number):    
+    output = ""
+    for printval in range(number):
+        output += f"The number is {printval}<br>\n"
+    return f"{output}"
+
+        
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '%':
+        result = num1 / num2
+    else:
+        return '<h1>Invalid operation</h1>'
+
+    return f'<h1>The result is: {result}</h1>'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+ 
